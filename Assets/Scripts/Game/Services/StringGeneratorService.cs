0516bf4701ad009bc7eaa632a514b255c16018ee
Tokari Shunya <tokari.shunya.s8@dc.tohoku.ac.jp>
2025-07-22 19:25:36 +0900

    文字列生成器を改良

--- Assets/Scripts/Game/Services/StringGeneratorService.cs
+++ Assets/Scripts/Game/Services/StringGeneratorService.cs
@@ -15,6 +15,8 @@ namespace Automan.Game.Service
     {
         private readonly CharacterList _characterList;
 
+        private readonly int _maxAttempt = 1000;
+
         /// <summary>
         /// コンストラクタ
         /// </summary>
@@ -35,64 +37,155 @@ namespace Automan.Game.Service
         /// <returns>文字列Modelの配列</returns>
         public AutomatonString[] Generate(int stringCount, int stateCount, (int Min, int Max) stringLengthRange, (int Min, int Max) checkedCharacterCountRange)
         {
-            int[] positiveStates = Enumerable.Range(0, stringCount).Where(_ => Random.value < 0.5f).ToArray();
-
-            Dictionary<(int State, AutomatonCharacter Character), int> transitions = new ();
-
+            // 文字を列挙
             AutomatonCharacter[] characters = _characterList.ToArray();
 
+            // オートマトンをランダムに生成
+            AutomatonModel automaton = GenerateRandomAutomaton(stateCount, characters);
+            AutomatonString[] newStrings = new AutomatonString[stringCount];
+
+            HashSet<string> positiveStrings = new ();
+            HashSet<string> negativeStrings = new ();
+
+            var positiveCount = stateCount / 2;
+            var negativeCount = stateCount / 2;
+
+            var count = 0;
+
+            if (positiveCount + negativeCount < stringCount)
+            {
+                if (Random.value < 0.5f)
+                {
+                    positiveCount++;
+                }
+                else
+                {
+                    negativeCount++;
+                }
+            }
+
+            for (int i = 0; i < _maxAttempt; i++)
+            {
+                var length = Random.Range(stringLengthRange.Min, stringLengthRange.Max + 1);
+                var checkedCharacterCount = Random.Range(checkedCharacterCountRange.Min, checkedCharacterCountRange.Max + 1);
+                
+                (string text, AutomatonString newString, bool isPositive) = GenerateRandomString(automaton, characters, length, checkedCharacterCount);
+
+                if (isPositive)
+                {
+                    if (!positiveStrings.Contains(text))
+                    {
+                        if (positiveCount > 0)
+                        {
+                            newStrings[count++] = newString;
+                            positiveCount--;
+                            positiveStrings.Add(text);
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    if (!negativeStrings.Contains(text))
+                    {
+                        if (negativeCount > 0)
+                        {
+                            newStrings[count++] = newString;
+                            negativeCount--;
+                            negativeStrings.Add(text);
+                        }
+                    }
+                }
+
+                if (count >= stringCount) break;
+            }
+
+            for (int i = 0; i < stringCount - count; i++)
+            {
+                var length = Random.Range(stringLengthRange.Min, stringLengthRange.Max + 1);
+                var checkedCharacterCount = Random.Range(checkedCharacterCountRange.Min, checkedCharacterCountRange.Max + 1);
+
+                (_, AutomatonString newString, _) = GenerateRandomString(automaton, characters, length, checkedCharacterCount);
+                newStrings[count++] = newString;
+            }
+
+            return newStrings;
+        }
+
+        private AutomatonModel GenerateRandomAutomaton(int stateCount, IEnumerable<AutomatonCharacter> characters)
+        {
+            HashSet<int> randomSet = Enumerable.Range(0, stateCount).Shuffle().Take(Random.Range(1, stateCount)).ToHashSet();
+
+            // 受理状態をランダムに設定
+            int[] positiveStates = Enumerable.Range(0, stateCount).Where(value => randomSet.Contains(value)).ToArray();
+
+            List<(int State, AutomatonCharacter Character)> transitionKeys = new ();
+
+            // 遷移のキーを列挙
             for (int i = 0; i < stateCount; i++)
             {
                 foreach (AutomatonCharacter character in characters)
                 {
-                    transitions[(i, character)] = Random.Range(0, stateCount);
+                    transitionKeys.Add((i, character));
                 }
             }
 
-            AutomatonModel automaton = new (stateCount, 0, positiveStates, transitions);
-            AutomatonString[] newStrings = new AutomatonString[stringCount];
+            (int State, AutomatonCharacter Character)[] shuffledKeys = transitionKeys.Shuffle().ToArray();
+            Dictionary<(int State, AutomatonCharacter Character), int> transitions = new ();
 
-            for (int i = 0; i < stringCount; i++)
+            // 遷移をランダムに設定
+            for (int i = 0; i < shuffledKeys.Length; i++)
             {
-                automaton.ResetState();
-
-                var length = Random.Range(stringLengthRange.Min, stringLengthRange.Max + 1);
-                AutomatonCharacter[] randomCharacters = new AutomatonCharacter[length];
-
-                for (int j = 0; j < length; j++)
+                if (i < stateCount)
                 {
-                    randomCharacters[j] = characters[Random.Range(0, characters.Length)];
+                    transitions[shuffledKeys[i]] = i;
                 }
+                else
+                {
+                    transitions[shuffledKeys[i]] = Random.Range(0, stateCount);
+                }
+            }
 
-                var checkedCount = Random.Range(checkedCharacterCountRange.Min, checkedCharacterCountRange.Max + 1);
-                HashSet<int> checkedCharacters = Enumerable.Range(0, length).Shuffle().Take(checkedCount - 1).Append(length).ToHashSet();
+            // オートマトンを生成
+            return new (stateCount, 0, positiveStates, transitions);
+        }
 
-                AutomatonString randomString = new (randomCharacters);
+        private (string stringText, AutomatonString automatonString, bool isPositive) GenerateRandomString(AutomatonModel automaton, IList<AutomatonCharacter> characters, int length, int checkedCharacterCount)
+        {
+            automaton.ResetState();
 
-                var index = 0;
-                List<AutomatonCharacter> newCharacters = new ();
+            AutomatonCharacter[] randomCharacters = new AutomatonCharacter[length];
+
+            for (int j = 0; j < length; j++)
+            {
+                randomCharacters[j] = characters[Random.Range(0, characters.Count)];
+            }
+
+            HashSet<int> checkedCharacters = Enumerable.Range(0, length).Shuffle().Take(checkedCharacterCount - 1).Append(length).ToHashSet();
+
+            AutomatonString randomString = new (randomCharacters);
+
+            var index = 0;
+            List<AutomatonCharacter> newCharacters = new ();
+
+            if (checkedCharacters.Contains(index++))
+            {
+                newCharacters.Add(automaton.IsInPositiveState ? _characterList.PositiveCharacter : _characterList.NegativeCharacter);
+            }
+
+            while (!randomString.IsEmpty)
+            {
+                newCharacters.Add(randomString.Peek());
+
+                automaton.Transition(randomString);
 
                 if (checkedCharacters.Contains(index++))
                 {
                     newCharacters.Add(automaton.IsInPositiveState ? _characterList.PositiveCharacter : _characterList.NegativeCharacter);
                 }
-
-                while (!randomString.IsEmpty)
-                {
-                    newCharacters.Add(randomString.Peek());
-
-                    automaton.Transition(randomString);
-
-                    if (checkedCharacters.Contains(index++))
-                    {
-                        newCharacters.Add(automaton.IsInPositiveState ? _characterList.PositiveCharacter : _characterList.NegativeCharacter);
-                    }
-                }
-
-                newStrings[i] = new AutomatonString(newCharacters);
             }
 
-            return newStrings;
+            return (randomString.ToString(), new AutomatonString(newCharacters), automaton.IsInPositiveState);
         }
     }
 }
